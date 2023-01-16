50958a67ac6b01776ffe1f4e464c979f9283a0fd
aquincho <aquinchon.test1@gmail.com>
2023-01-16 13:32:48 +0100

    cpp01 ex01

--- cpp00/ex02/Account.hpp
+++ cpp00/ex02/Account.hpp
@@ -13,6 +13,7 @@
 #define __ACCOUNT_H__
 
 # include <iostream>
+# include <string>
 
 // ************************************************************************** //
 //                               Account Class                                //
